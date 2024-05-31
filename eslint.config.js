dce0331da62615c9f32678c9692f8a846bfea168
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-31 12:34:19 -0700

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import stylistic from "@stylistic/eslint-plugin";
+import ts from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import Lint from "@jimbojet/lint";
+
+export default [
+  ...new Lint(
+    {
+      stylistic,
+      files: ["*.config.js"],
+    },
+    {
+      ts,
+      parser,
+      files: ["src/**/*.ts"],
+    },
+  ).configs,
+];
