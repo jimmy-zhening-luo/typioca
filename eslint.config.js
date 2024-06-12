f0c0dcdfae75326ed9630b68900300f2d216b032
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-11 23:08:37 -0700

    update lint, lintscripts, lint/tsc config files; upscope types to include non d.ts source files

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,20 @@ import Lint from "@jimbojet/lint";
 export default [
   ...new Lint(
     {
-      js: ["*.config.js"],
+      js: [
+        // "*.config.js",
+        "eslint.config.js",
+      ],
       ts: [
         // "*.config.ts",
         "src/**/*.ts",
         "types/**/*.d.ts",
       ],
-      svelte: ["src/**/*.svelte"],
-      html: ["src/**/*.html"],
       jsonc: ["tsconfig.json"],
-      json: ["package.json"],
+      json: [
+        // "*.json",
+        "package.json",
+      ],
       yml: [".github/workflows/*.yml"],
     },
   )
